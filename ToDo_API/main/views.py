56cdf157b399bc937d800c1f140e7516dc929db9
jacurlive <jasurkkirchh@gmail.com>
2023-12-11 10:49:04 +0500

    add comment to views

--- ToDo_API/main/views.py
+++ ToDo_API/main/views.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import ToDo
 from .serializers import ToDoSerializer
 
 
+# Get list and Create
 class ToDoView(generics.ListCreateAPIView):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
 
 
+# Get detail and Delete with pk
 class ToDoDestroy(generics.RetrieveDestroyAPIView):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
 
 
-class ToDoUpdate(generics.UpdateAPIView):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
+# Update
+# class ToDoUpdate(generics.UpdateAPIView):
+#     queryset = ToDo.objects.all()
+#     serializer_class = ToDoSerializer
 
 
+# Get detail and Update with pk
 class ToDoDetail(generics.RetrieveUpdateAPIView):
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
-
