ab9d68e0d5559a466d3efedc64b1255fbc12692a
jacurlive <jasurkkirchh@gmail.com>
2024-01-15 11:01:01 +0500

    add user folder to admin panel

--- main/admin.py
+++ main/admin.py
@@ -4,13 +4,13 @@ from .models import ToDo, Folder
 
 @admin.register(ToDo)
 class ToDoAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "completed", "folder")
+    list_display = ("id", "title", "completed", "user", "folder")
     list_display_links = ("id", "title")
     list_editable = ("completed", "folder")
+    readonly_fields = ("user",)
 
 
 @admin.register(Folder)
 class FolderAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     list_display_links = ("id", "name")
-
